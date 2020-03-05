0179b2e8767ec2cb157525488e45ca274df55d16
Guillaume Rose <guillaume.rose@docker.com>
2020-03-05 15:59:58 +0100

    Add basic crawler
    Signed-off-by: Guillaume Rose <guillaume.rose@docker.com>

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var maxURLs int
+
 func main() {
+	flag.IntVar(&maxURLs, "max", 20, "maximum number of URLs in sitemap")
 	flag.Parse()
 	if flag.NArg() != 1 {
 		logrus.Fatal("url is mandatory")
 	}
-	if err := run(flag.Arg(0)); err != nil {
+	if err := run(flag.Arg(0), maxURLs); err != nil {
 		logrus.Fatal(err)
 	}
 }
 
-func run(base string) error {
-	links, err := scrape(base)
+func run(base string, maxURLs int) error {
+	links, err := crawl(base, maxURLs)
 	if err != nil {
 		return err
 	}
--- scrape.go
+++ scrape.go
@@ -5,18 +5,21 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"sort"
 	"strings"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 var linksExtractor = regexp.MustCompile(`<a\s+(?:[^>]*?\s+)?href="([^"]*)"`)
 
-func scrape(base string) ([]string, error) {
+func scrape(url string) ([]string, error) {
 	client := http.Client{
 		Timeout: 5 * time.Second,
 	}
 
-	req, err := http.NewRequest("GET", base, nil)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -41,5 +44,40 @@ func scrape(base string) ([]string, error) {
 			links = append(links, link)
 		}
 	}
+	return links, nil
+}
+
+func crawl(base string, maxURLs int) ([]string, error) {
+	visited := make(map[string]struct{})
+	queue := []string{"/"}
+	for len(queue) > 0 && len(visited) < maxURLs {
+		current := queue[0]
+		queue = queue[1:]
+
+		if _, ok := visited[current]; ok {
+			continue
+		}
+		visited[current] = struct{}{}
+
+		url := base + current
+		logrus.Infof("Visiting %s", url)
+		links, err := scrape(url)
+		if err != nil {
+			logrus.Warnf("Error while scraping %s: %v", url, err)
+			continue
+		}
+
+		for _, link := range links {
+			if _, ok := visited[link]; !ok {
+				queue = append(queue, link)
+			}
+		}
+	}
+
+	var links []string
+	for link := range visited {
+		links = append(links, link)
+	}
+	sort.Strings(links)
 	return links, nil
 }
